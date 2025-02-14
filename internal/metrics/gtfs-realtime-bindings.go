452c240e7298ee4064211af71a3360810396a7ab
Rohan Hasabe <rohanhasabe8@gmail.com>
2025-02-14 01:21:57 -0500

    metrics: Add Vehicle Position Validation

--- internal/metrics/gtfs-realtime-bindings.go
+++ internal/metrics/gtfs-realtime-bindings.go
@@ -102,3 +102,73 @@ func CheckVehicleCountMatch(server models.ObaServer) error {
 
 	return nil
 }
+
+func isValidLatLong(lat, long float64) bool {
+	return lat >= -90 && lat <= 90 && long >= -180 && long <= 180
+}
+
+func CountVehiclePosition(server models.ObaServer) (int, error) {
+	parsedURL, err := url.Parse(server.VehiclePositionUrl)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse GTFS-RT URL: %v", err)
+	}
+
+	req, err := http.NewRequest("GET", parsedURL.String(), nil)
+	if err != nil {
+		return 0, fmt.Errorf("failed to create HTTP request: %v", err)
+	}
+	if server.GtfsRtApiKey != "" && server.GtfsRtApiValue != "" {
+		req.Header.Set(server.GtfsRtApiKey, server.GtfsRtApiValue)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		sentry.CaptureException(err)
+		return 0, fmt.Errorf("failed to fetch GTFS-RT feed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, fmt.Errorf("failed to read GTFS-RT feed: %v", err)
+	}
+
+	realTimeData, err := gtfs.ParseRealtime(data, &gtfs.ParseRealtimeOptions{})
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse GTFS-RT feed: %v", err)
+	}
+
+	totalCount := len(realTimeData.Vehicles)
+	invalidCount := 0
+
+	//Count invalid postions
+	for _, vehicle := range realTimeData.Vehicles {
+		fmt.Println(float64(*vehicle.Position.Latitude), float64(*vehicle.Position.Longitude))
+		if !isValidLatLong(float64(*vehicle.Position.Latitude), float64(*vehicle.Position.Longitude)) {
+			invalidCount++
+		}
+	}
+
+	// Update metrics
+	RealtimeVehiclePositions.WithLabelValues(
+		server.VehiclePositionUrl,
+		strconv.Itoa(server.ID),
+	).Set(float64(totalCount))
+
+	InvalidVehiclePositions.WithLabelValues(
+		server.VehiclePositionUrl,
+		strconv.Itoa(server.ID),
+	).Set(float64(invalidCount))
+
+	if totalCount > 0 {
+		validPercent := float64(totalCount-invalidCount) / float64(totalCount) * 100
+		ValidVehiclePositionsPercent.WithLabelValues(
+			server.VehiclePositionUrl,
+			strconv.Itoa(server.ID),
+		).Set(validPercent)
+	}
+
+	return totalCount, nil
+
+}
