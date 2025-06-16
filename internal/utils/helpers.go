a1bf3ebcae69235e4c8a69ea7ad794c9c17a055d
Abo-Omar-74 <mohamedaboomar1211@gmail.com>
2025-06-16 04:46:20 +0300

    docs(utils): add documentation for MakeMap helper function

--- internal/utils/helpers.go
+++ internal/utils/helpers.go
@@ -1,5 +1,6 @@
 package utils
 
+// MakeMap creates and returns a map[string]string containing a single key-value pair.
 func MakeMap(key, value string) map[string]string {
 	return map[string]string{key: value}
 }
